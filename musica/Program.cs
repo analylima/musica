e6810bf76d82cbae5c973a1c843b258cc28331ac
analylima <162760535+analylima@users.noreply.github.com>
2024-03-08 21:49:02 -0300

    Add files via upload

---
+++ musica/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("De janeiro a Janeiro!");
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("Não consigo olhar no fundo dos seus olhos\n"+
+"E enxergar as coisas que me deixam no ar\n"+
+"Me deixam no ar\n"+
+"As várias fases e estações que me levam com o vento\n"+
+"E o pensamento bem devagar\n"+
+"Outra vez, eu tive que fugir\n"+
+"Eu tive que correr, pra não me entregar\n"+
+"As loucuras que me levam até você\n"+
+"Me fazem esquecer que eu não posso chorar\n"+
+"Olhe bem no fundo dos meus olhos\n"+
+"E sinta a emoção que nascerá quando você me olhar\n"+
+"O universo conspira a nosso favor\n"+
+"A consequência do destino é o amor\n"+
+"Pra sempre vou te amar\n"+
+"Mas talvez, você não entenda\n"+
+"Essa coisa de fazer o mundo acreditar\n"+
+"Que o meu amor\n"+
+"Não será passageiro\n"+
+"Te amarei de janeiro a janeiro");
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("Até o mundo acabar\n"+
+"Até o mundo acabar\n"+
+"Até o mundo acabar\n"+
+"Até o mundo acabar");
+Console.ForegroundColor = ConsoleColor.White;
+
+Console.WriteLine("Mas talvez, você não entenda\n"+
+"Essa coisa de fazer o mundo acreditar\n"+
+"Que o meu amor\n"+
+"Não será passageiro\n"+
+"Te amarei de janeiro a janeiro");
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("Até o mundo acabar\n"+
+"Até o mundo acabar\n"+
+"Até o mundo acabar\n"+
+"Até o mundo acabar");
+Console.ForegroundColor = ConsoleColor.White;
+
+Console.WriteLine("De janeiro a janeiro");
